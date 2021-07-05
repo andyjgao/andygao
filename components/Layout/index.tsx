27456022eb8694115903c9b0035c9530b3ea2b4a
Andy Gao <supagao7@gmail.com>
2021-07-04 23:12:58 -0400

    modify

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,9 +3,10 @@ import { NavBar } from "../NavBar";
 
 export const Layout: FunctionComponent = ({ children }) => {
   return (
-    <div className="container mx-auto max-w-screen-md px-4">
+    <div className="flex flex-col h-screen container mx-auto max-w-screen-md px-4">
       <NavBar />
-      {children}
+      <div className="h-10 mb-auto">{children}</div>
+      {/* <footer className="h-10">Twitter</footer> */}
     </div>
   );
 };
--- pages/links.tsx
+++ pages/links.tsx
@@ -1,5 +1,6 @@
 const Links = () => (
   <>
+    <h1 className="text-2xl pb-8">Links</h1>
     The following is a curation of my favorite links, books, resources over the
     years.
   </>
